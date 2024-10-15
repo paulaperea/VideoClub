98397dfee01879262d54e13767aceb1d04d3c684
PaulaMPerea <paula.m.perea@gmail.com>
2024-10-15 14:44:57 +0200

    Add files via upload

---
+++ app/soporte.php
@@ -0,0 +1,29 @@
+<?php
+
+class Soporte {
+    
+    private static $iva = 21; 
+    
+    public function __construct(
+        public $titulo,
+        protected $numero,
+        private $precio,
+    ) {}
+
+    public function getPrecio() {
+        return $this->precio;
+    }
+
+    public function getNumero() {
+        return $this->numero;
+    }
+
+    public function getPrecioConIva() {
+        return $this->precio + (self::$iva * $this->precio);
+    }
+
+    public function muestraResumen () { 
+    }
+}
+
+?>
